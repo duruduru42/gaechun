1d580fb51e2501a9d03db4ea2c6378ac212ab42b
duruduru42 <khm56422@naver.com>
2024-11-13 15:12:57 +0900

    '수정3'

--- app/confirm/page.jsx
+++ app/confirm/page.jsx
@@ -1,14 +1,13 @@
+import { useSearchParams } from 'next/navigation';
 
-export default function Signup({
-  searchParams,
-}: {
-  searchParams: { message: string };
-}) {
+export default function Signup() {
+  const searchParams = useSearchParams();
+  const message = searchParams.get('message'); // Access 'message' directly
+
   return (
     <div>
-
       <div className="w-full px-8 sm:max-w-lg mx-auto mt-8">
-        <p className="text-foreground">{searchParams.message}</p>
+        <p className="text-foreground">{message}</p>
       </div>
     </div>
   );
