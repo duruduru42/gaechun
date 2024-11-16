21f3b911f8f1719547278ca9a00bd1c883fe480a
duruduru42 <khm56422@naver.com>
2024-11-16 21:08:38 +0900

    'seo2'

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,10 +7,12 @@ export const metadata = {
   openGraph: {
     type: 'website',
     title: '개천용 컨설팅',
+    site_name : '개천용 컨설팅',
+    image : 'www.gaechundragon.co.kr/components/opengraph.png',
     description: '농어촌 전형, 기회균형 전형, 고른기회 전형, 특성화고 전형 전문으로 컨설팅을 하고 있는 개천용 컨설팅입니다. 정시 컨설팅과 수시 컨설팅, 생기부 컨설팅까지 모두 진행하고 있습니다.',
   },
   description: "농어촌 전형, 기회균형 전형, 고른기회 전형, 특성화고 전형 전문으로 컨설팅을 하고 있는 개천용 컨설팅입니다. 정시 컨설팅과 수시 컨설팅, 생기부 컨설팅까지 모두 진행하고 있습니다.",
-  keywords: '개천용입시컨설팅 농어촌전형 기회균형전형 고른기회전형 고른기회전형컨설팅 기회균형전형컨설팅 특성화고전형',
+  keywords: '개천용컨설팅 농어촌전형 기회균형전형 고른기회전형 고른기회전형컨설팅 기회균형전형컨설팅 특성화고전형',
   icons: {
     icon: "/icon.ico",
   },
