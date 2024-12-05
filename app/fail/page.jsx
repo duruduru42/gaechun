e14983266cf532f6467ce01335493ee3f141ab28
duruduru42 <khm56422@naver.com>
2024-12-05 13:16:56 +0900

    '수정'

--- app/fail/page.jsx
+++ app/fail/page.jsx
@@ -2,18 +2,16 @@
 
 import { useSearchParams } from "next/navigation";
 
-export function FailPage() {
-    const [searchParams] = useSearchParams();
-  
+export default function FailPage() {
+    const searchParams = useSearchParams();
+
     return (
-      <div className="result wrapper">
-        <div className="box_section">
-          <h2>
-            결제 실패
-          </h2>
-          <p>{`에러 코드: ${searchParams.get("code")}`}</p>
-          <p>{`실패 사유: ${searchParams.get("message")}`}</p>
+        <div className="result wrapper">
+            <div className="box_section">
+                <h2>결제 실패</h2>
+                <p>{`에러 코드: ${searchParams.get("code")}`}</p>
+                <p>{`실패 사유: ${searchParams.get("message")}`}</p>
+            </div>
         </div>
-      </div>
     );
-  }
+}
