4b0e64f9045437c0bb9538f16d5310078730d402
duruduru42 <khm56422@naver.com>
2025-12-04 22:42:50 +0900

    '12/8'

--- components/First/index.js
+++ components/First/index.js
@@ -29,7 +29,7 @@ const Hero = () => {
         </p>
         <button 
         onClick={() => {
-          alert("준비중입니다.")
+          alert("12/8일 오픈 예정입니다.")
          }}
         className="rounded-2xl px-6 py-3 text-lg font-bold mt-10 mb-24 bg-black text-white">
           바로가기
